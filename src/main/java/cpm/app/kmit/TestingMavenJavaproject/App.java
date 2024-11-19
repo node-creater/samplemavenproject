04193345f653b66cc70039f57f088a6b9dc4b73f
node-creater <preetham.a.annadanam@gmail.com>
2024-11-20 02:01:01 +0530

    sample maven java

---
+++ src/main/java/cpm/app/kmit/TestingMavenJavaproject/App.java
@@ -0,0 +1,13 @@
+package cpm.app.kmit.TestingMavenJavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Preetham 22bd1a6623" );
+    }
+}
